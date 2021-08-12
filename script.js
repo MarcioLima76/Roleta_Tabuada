155c80ac8934f6338cae175ee3293068420e0567
MARCIO NOGUEIRA DE LIMA <marciolima76@hotmail.com>
2021-08-12 10:21:08 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let button1 = document.getElementById("generate1");
+button1.onclick = function gerenate() {
+    let randomNum1 = Math.floor(Math.random() * 10 + 0);
+    let el1 = document.getElementById("ranNum1");
+    el1.innerHTML = randomNum1;
+};
+
+let button2 = document.getElementById("generate2");
+button2.onclick = function gerenate() {
+    let randomNum2 = Math.floor(Math.random() * 10 + 0);
+    let el2 = document.getElementById("ranNum2");
+    el2.innerHTML = randomNum2;
+
+};
+
+
+function verificar() {
+    let n1 = Number(document.querySelector("#ranNum1").innerHTML);
+    let n2 = Number(document.querySelector("#ranNum2").innerHTML);
+
+    let resultado = (n1) * (n2);
+    document.querySelector(".resultado").innerHTML = resultado;
+
+}
+
+
+function capturar() {
+    let capt = document.querySelector("#resposta").value;
+    document.querySelector('.valorDigitado').innerHTML = capt;
+    if (valorDigitado === resultado) {
+        capt.innerHTML += `<p>Parabéns! Você acertou!</p>`
+    } else {
+        capt.innerHTML += `<p>("Errado!Tente novamente")</p>`
+    }
+
+}
